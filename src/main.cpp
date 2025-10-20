7d3da939f6332238cc18c5d8d141a35e5d273b44
tavichh <me@tavichh.com>
2025-10-20 02:40:23 -0400

    added vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include "window/window.hpp"
 #include <error_codes.hpp>
 int main() {
+  window *game_window = new window();
   return static_cast<int>(error_codes::SUCCESS);
 }
--- src/window/window.hpp
+++ src/window/window.hpp
@@ -1,13 +1,14 @@
 
 #ifndef PALADIN98_WINDOW_HPP
 #define PALADIN98_WINDOW_HPP
+#include <cstdint>
 
 class window {
 public:
   window();
   ~window();
 private:
-
+  void create_window(uint32_t width, uint32_t height, const char *name);
 };
 
 #endif // PALADIN98_WINDOW_HPP
